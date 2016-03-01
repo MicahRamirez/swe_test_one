bdb47c60d52b6cd34e93bbe2bdedc44aa85484a9
Xavier Ramirez <alpha@wireless-10-145-153-134.public.utexas.edu>
2016-03-01 15:17:17 -0600

    Adding RangeIterator Tests and interface

---
+++ RangeIterator.py
@@ -0,0 +1,13 @@
+""" Implement a range iterator """
+class range_iterator_class () :
+
+	def __init__(self, start,end):
+
+	def __iter__(self):
+
+	def __next__(self):
+
+
+""" Implement a range iterator function that has similar behavior to the range_iterator """ 
+def range_iterator_function(b,e):
+
